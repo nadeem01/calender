5dcb5f2a63fe9e2a1899af5f7b3d7111cbbd8004
nadeem abbas <na.abbas001@gmail.com>
2022-12-26 23:42:35 +0500

    some changes

--- src/components/dummyData.js
+++ src/components/dummyData.js
@@ -1,110 +1,66 @@
-
-// export const duration = [
-//   {"id":0,
-//   "time":"09:00 am"},
-//   {"id":1,
-//   "time":"09:30 am"},
-//   {"id":2,
-//   "time":"10:00 am"},
-//   {"id":3,
-//   "time":"10:30 am"},
-//   {"id":4,
-//   "time":"11:00 am"},
-//   {"id":5,
-//   "time":"11:30 am"},
-//   {"id":6,
-//   "time":"12:00 am"},
-//   {"id":7,
-//   "time":"12:30 am"},
-//  ]
-// export const employees = {
-  
-    
-//     1: "Rija Riaz",
-   
-  
-  
-    
-//     2: "Ahmed Raza",
-   
-  
-  
-    
-//     3: "Ayesha Zaka",
-   
-  
-  
-    
-//     4: "Hamayun jawad",
-   
-  
-  
-    
-//     5: "Ali Raza",
-   
-  
-  
-    
-//     6: "Aiman Akram",
-   
-  
-  
-    
-//     7: "Azka mehmood",
-   
-  
-// }
-// export const data =
-// [
-//   {
-//     "id":0,
-//  "name":"Rija Riaz"},
-//  {"id":1,
-//  "name":"Ahmed Raza"},
-//  {"id":2,
-//  "name":"Ayesha Zaka"},
-//  {"id":3,
-//  "name":"Hamayun jawad"},
-//  {"id":4,
-//  "name":"Ali Razaz"},
-//  {"id":5,
-//  "name":"Aiman Akram"},
-//  {"id":6,
-//  "name":"Azka mehmood"},]
  export const data= {
-    "employees":[
+    employees:[
       {
-        "id":0,
-     "name":"Rija Riaz"},
-     {"id":1,
-     "name":"Ahmed Raza"},
-     {"id":2,
-     "name":"Ayesha Zaka"},
-     {"id":3,
-     "name":"Hamayun jawad"},
-     {"id":4,
-     "name":"Ali Razaz"},
-     {"id":5,
-     "name":"Aiman Akram"},
-     {"id":6,
-     "name":"Azka mehmood"},],
-    "duration":[
-      {"id":0,
-      "time":"09:00 am"},
-      {"id":1,
-      "time":"09:30 am"},
-      {"id":2,
-      "time":"10:00 am"},
-      {"id":3,
-      "time":"10:30 am"},
-      {"id":4,
-      "time":"11:00 am"},
-      {"id":5,
-      "time":"11:30 am"},
-      {"id":6,
-      "time":"12:00 am"},
-      {"id":7,
-      "time":"12:30 am"},
+        id:0,  
+        name:"Rija Riaz"
+      },
+     {
+      id:1,   
+      name:"Ahmed Raza"
+    },
+     {
+      id:2,   
+      name:"Ayesha Zaka"
+    },
+     {
+      id:3,   
+      name:"Hamayun jawad"
+    },
+     {
+      id:4,   
+      name:"Ali Razaz"
+    },
+     {
+      id:5,   
+      name:"Aiman Akram"
+    },
+     {
+      id:6,   
+      name:"Azka mehmood"
+    }],
+    duration:[
+      {
+        id:0,
+        time:"09:00 am"
+      },
+      {
+        id:1,
+        time:"09:30 am"
+      },
+      {
+        id:2,
+        time:"10:00 am"
+      },
+      {
+        id:3,
+        time:"10:30 am"
+      },
+      {
+        id:4,
+        time:"11:00 am"
+      },
+      {
+        id:5,
+        time:"11:30 am"
+      },
+      {
+        id:6,
+        time:"12:00 am"
+      },
+      {
+        id:7,
+        time:"12:30 am"
+      },
      ],
      appointments:[
       {
